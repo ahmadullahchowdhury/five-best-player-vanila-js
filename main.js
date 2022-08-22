e737cb2223e3e07b0a2c36e799b715308fdf30ec
kowchergit config --global user.email kowcher99@gmail.com <37480985+kowcher99@users.noreply.github.com>
2022-08-22 19:42:21 +0600

    ready

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function listItemCounter(){
     var liList = document.getElementById("myul").getElementsByTagName("li");
-    var largo = liList.length
-    return largo
+    var listLen = liList.length
+    return listLen
 }
 
 function getPlayerName(playerId){
@@ -13,71 +13,100 @@ function addListItem(text){
     const list = document.getElementById("myul")
     const li = document.createElement('li')
     li.innerText = text
-    list.appendChild(li)
+    list.appendChild(li)  
 }
 
 function buttonDisable(btnId){
     const btn =  document.getElementById(btnId)
     btn.setAttribute('disabled', true)
+    btn.style.backgroundColor = 'DarkGray';
+}
+
+function perPlayerExpense(){
+    const len = listItemCounter()
+    const perPlayerInputField =  document.getElementById("per-player-exp")
+    const perPlayerInputFieldToInt = parseInt(perPlayerInputField.value)
+    if( perPlayerInputField.value === '') {
+        alert("Put Per Player Amount To Calculate")
+    }{
+        const totalPlayerExpense = perPlayerInputFieldToInt*len
+        return totalPlayerExpense
+    }
+}
+
+function totalTeamExpense(){
+    const playerExpense =  perPlayerExpense()
+
+    const managerInputField =  document.getElementById("manager-exp")
+    const managerInputFieldToInt = parseInt(managerInputField.value)
+
+    const coachInputField =  document.getElementById("coach-exp")
+    const coachInputFieldToInt = parseInt(coachInputField.value)
+
+    if(managerInputField.value === ''){
+        alert("Put Manager Coach Amount To Calculate")
+    }
+
+    let totalExpense = playerExpense + managerInputFieldToInt + coachInputFieldToInt
+
+    return totalExpense
+}
+
+function playerLeftCounter(){
+    const listLength = listItemCounter()
+    const playerLeftCountElm = document.getElementById("player-left-count")
+    playerLeftCountElm.innerText = ((listLength+2)+5)-((listLength+1)+(listLength+1))
+}
+
+function addPlayerName(playerId, buttonId){
+    const listLen = listItemCounter()
+    if(listLen >= 5){
+        const mainDiv = document.getElementById('selected-player-div')
+        const p  = document.createElement('p')
+        p.innerHTML =`<p class="mt-1 text-red-600 text-center">No Player Left!!</p>`
+        mainDiv.appendChild(p)
+        buttonDisable(buttonId)
+    }else{
+    addListItem(getPlayerName(playerId))
+    buttonDisable(buttonId)
+    }
+    playerLeftCounter()
+
 }
 
 document.getElementById('btn-1').addEventListener('click', function(){
-    const largo = listItemCounter()
-    if(largo >= 5){
-        alert('you exceed player limit')
-    }else{
-    addListItem(getPlayerName('player-name-1'))
-    buttonDisable('btn-1')
-    }
+    addPlayerName('player-name-1','btn-1')
 })
 
 document.getElementById('btn-2').addEventListener('click', function(){
-    const largo = listItemCounter()
-    if(largo >= 5){
-        alert('you exceed player limit')
-    }else{
-    addListItem(getPlayerName('player-name-2'))
-    buttonDisable('btn-2')
-    }
+    addPlayerName('player-name-2','btn-2')
 })
 
 document.getElementById('btn-3').addEventListener('click', function(){
-    const largo = listItemCounter()
-    if(largo >= 5){
-        alert('you exceed player limit')
-    }else{
-    addListItem(getPlayerName('player-name-3'))
-    buttonDisable('btn-3')
-    }
+    addPlayerName('player-name-3','btn-3')
+
 })
 
 document.getElementById('btn-4').addEventListener('click', function(){
-    // const playerNameText =  getPlayerName('player-name-4')
-    const largo = listItemCounter()
-    if(largo >= 5){
-        alert('you exceed player limit')
-    }else{
-    addListItem(getPlayerName('player-name-4'))
-    buttonDisable('btn-4')
-    }
+    addPlayerName('player-name-4','btn-4')
 })
 
 document.getElementById('btn-5').addEventListener('click', function(){
-    const largo = listItemCounter()
-    if(largo >= 5){
-        alert('you exceed player limit')
-    }else{
-    addListItem(getPlayerName('player-name-5'))
-    buttonDisable('btn-5')
-    }
+    addPlayerName('player-name-5','btn-5')
 })
 
 document.getElementById('btn-6').addEventListener('click', function(){
-    const largo = listItemCounter()
-    if(largo >= 5){
-        alert('you exceed player limit')
-    }else{
-    addListItem(getPlayerName('player-name-6'))
-    buttonDisable('btn-6')
-    }
+    addPlayerName('player-name-6','btn-6')
+})
+
+document.getElementById('player-cal-btn').addEventListener('click', function(){
+    const playerExpense =  perPlayerExpense()
+    const playerExpenseTextElm =  document.getElementById('player-expense')
+    playerExpenseTextElm.innerText = playerExpense
+})
+
+document.getElementById('total-cal-btn').addEventListener('click', function(){
+    const totalExpenseValue =  totalTeamExpense()
+    const totalExpenseTextElm =  document.getElementById('total-expense')
+    totalExpenseTextElm.innerText = totalExpenseValue
 })
